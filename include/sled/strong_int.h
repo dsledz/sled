438a417d2c9f06ee3d6f4df02761263144b41b30
Dan Sledz <dsledz@gmail.com>
2019-01-06 23:34:42 +0000

    Add a CRTP StrongInt alternative

--- include/sled/strong_int.h
+++ include/sled/strong_int.h
@@ -154,14 +154,129 @@ class StrongInteger {
 };
 
 template <typename T, typename Tag>
-std::ostream &operator<<(std::ostream &os, const StrongInteger<T, Tag> &d) {
-  os << static_cast<T>(d);
-  return os;
-}
-
-template <typename T, typename Tag>
 struct __is_wrapped_integer_helper<StrongInteger<T, Tag>>
     : public std::true_type {};
+
+/**
+ * Strong Int template.
+ *
+ * Example usage:
+ * struct DomainInt : StrongInt<uint32_t, DomainInt> { }
+ *
+ * Strong Integers are used when it's desirable to have a strong type that can
+ * be represented by an integral value but you want to restrict operations
+ * between other types.
+ */
+template <typename T, typename Tag>
+class StrongInt {
+ public:
+  StrongInt() = default;
+  template <typename Ta>
+  explicit constexpr StrongInt(Ta v) : v(static_cast<T>(v)) {}
+
+  T v{0};
+
+  explicit constexpr operator T() const noexcept { return v; }
+
+  constexpr T &get() noexcept { return v; }
+
+  constexpr T get() const noexcept { return v; }
+
+  a_forceinline constexpr Tag operator+(Tag const &rhs) const noexcept {
+    return Tag(v + rhs.v);
+  }
+
+  a_forceinline constexpr Tag operator-(Tag const &rhs) const noexcept {
+    return Tag(v - rhs.v);
+  }
+
+  a_forceinline constexpr Tag operator*(Tag const &rhs) const noexcept {
+    return Tag(v * rhs.v);
+  }
+
+  a_forceinline constexpr Tag operator/(Tag const &rhs) const noexcept {
+    return Tag(v / rhs.v);
+  }
+
+  a_forceinline constexpr Tag operator%(Tag const &rhs) const noexcept {
+    return Tag(v % rhs.v);
+  }
+
+  a_forceinline constexpr Tag operator+=(Tag const &rhs) noexcept {
+    Tag &t = static_cast<Tag &>(*this);
+    t.v += rhs.v;
+    return t;
+  }
+
+  a_forceinline constexpr Tag operator-=(Tag const &rhs) noexcept {
+    Tag &t = static_cast<Tag &>(*this);
+    t.v -= rhs.v;
+    return t;
+  }
+
+  a_forceinline constexpr Tag operator*=(Tag const &rhs) noexcept {
+    Tag &t = static_cast<Tag &>(*this);
+    t.v *= rhs.v;
+    return t;
+  }
+
+  a_forceinline constexpr Tag operator/=(Tag const &rhs) noexcept {
+    Tag &t = static_cast<Tag &>(*this);
+    t.v /= rhs.v;
+    return t;
+  }
+
+  a_forceinline constexpr Tag operator%=(Tag const &rhs) noexcept {
+    Tag &t = static_cast<Tag &>(*this);
+    t.v %= rhs.v;
+    return t;
+  }
+
+  a_forceinline constexpr explicit operator bool() const noexcept {
+    return v != 0;
+  }
+
+  a_forceinline constexpr bool operator>(Tag const &rhs) const noexcept {
+    return v > rhs.v;
+  }
+  a_forceinline constexpr bool operator>=(Tag const &rhs) const noexcept {
+    return v >= rhs.v;
+  }
+  a_forceinline constexpr bool operator<(Tag const &rhs) const noexcept {
+    return v < rhs.v;
+  }
+  a_forceinline constexpr bool operator<=(Tag const &rhs) const noexcept {
+    return v <= rhs.v;
+  }
+  a_forceinline constexpr bool operator==(Tag const &rhs) const noexcept {
+    return v == rhs.v;
+  }
+  a_forceinline constexpr bool operator!=(Tag const &rhs) const noexcept {
+    return v != rhs.v;
+  }
+  static a_forceinline constexpr Tag minimum(Tag const &a) noexcept {
+    return a;
+  }
+  static a_forceinline Tag minimum(Tag const &a, Tag const &b) noexcept {
+    return a < b ? a : b;
+  }
+  template <typename... Ta>
+  static a_forceinline Tag minimum(Tag const &a, Tag const &b,
+                                   Ta const &... t) noexcept {
+    return minimum(a, minimum(b, t...));
+  }
+  static a_forceinline constexpr Tag maximum(Tag const &a) noexcept {
+    return a;
+  }
+  static a_forceinline Tag maximum(Tag const &a, Tag const &b) noexcept {
+    return a > b ? a : b;
+  }
+  template <typename... Ta>
+  static a_forceinline Tag maximum(Tag const &a, Tag const &b,
+                                   Ta const &... t) noexcept {
+    return maximum(a, maximum(b, t...));
+  }
+};
 
 };  // namespace sled
 
---
+++ src/lib/strong_int_test.cpp
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2018, Dan Sledz
+ * All rights reserved.
+ * Licensed under BSD-2-Clause license.
+ */
+
+#include "sled/strong_int.h"
+
+#include "gtest/gtest.h"
+
+class StrongIntegerTest : public ::testing::Test {
+ protected:
+  StrongIntegerTest() = default;
+
+  void SetUp() override {}
+  void TearDown() override {}
+};
+
+struct TestInt : public sled::StrongInt<uint32_t, TestInt> {
+  using sled::StrongInt<uint32_t, TestInt>::StrongInt;
+
+  friend std::ostream &operator<<(std::ostream &os, TestInt const &rhs) {
+    os << static_cast<uint32_t>(rhs.v);
+    return os;
+  }
+};
+
+TEST_F(StrongIntegerTest, inherited) {
+  TestInt ti1{100};
+  TestInt ti2{10};
+
+  EXPECT_EQ(100, ti1.get());
+
+  static_assert(std::is_same_v<decltype(ti1 + ti2), decltype(ti1)>,
+                "Wrong deduced type");
+  static_assert(std::is_same_v<decltype(ti1 - ti2), decltype(ti1)>,
+                "Wrong deduced type");
+  static_assert(std::is_same_v<decltype(ti1 * ti2), decltype(ti1)>,
+                "Wrong deduced type");
+  static_assert(std::is_same_v<decltype(ti1 / ti2), decltype(ti1)>,
+                "Wrong deduced type");
+  static_assert(std::is_same_v<decltype(ti1 % ti2), decltype(ti1)>,
+                "Wrong deduced type");
+
+  static_assert(std::is_same_v<decltype(ti1 += ti2), decltype(ti1)>,
+                "Wrong deduced type");
+  static_assert(std::is_same_v<decltype(ti1 -= ti2), decltype(ti1)>,
+                "Wrong deduced type");
+  static_assert(std::is_same_v<decltype(ti1 *= ti2), decltype(ti1)>,
+                "Wrong deduced type");
+  static_assert(std::is_same_v<decltype(ti1 /= ti2), decltype(ti1)>,
+                "Wrong deduced type");
+  static_assert(std::is_same_v<decltype(ti1 %= ti2), decltype(ti1)>,
+                "Wrong deduced type");
+
+  ti1 += ti2;
+  EXPECT_EQ(110, ti1.get());
+
+  std::stringstream ss;
+  ss << ti1;
+  EXPECT_EQ("110", ss.str());
+}
